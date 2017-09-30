584a143a638be6232cd2193053f9dd91c57283e5
huskyhoochu <dfg1499@gmail.com>
2017-09-30 15:28:26 +0900

    blog 어플리케이션 정의

---
+++ huskyblog/blog/models.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.db import models
+
+class Post(models.Model):
+    """
+    Post 모델을 정의한다
+    1. 글쓴이 (author)
+    2. 제목 (title)
+    3. 내용 (content)
+    4. 생성일 (created_date)
+    5. 게시일 (published_date)
+    """
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    title = models.CharField(max_length=100)
+    content = models.TextField(blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
