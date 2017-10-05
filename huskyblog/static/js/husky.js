26e8a45192dfd0c87b6d16b37203e83ffbf628dc
huskyhoochu <dfg1499@gmail.com>
2017-10-05 15:49:22 +0900

    헤더 투명도 조절

--- huskyblog/static/js/husky.js
+++ huskyblog/static/js/husky.js
@@ -1,13 +1,10 @@
+//메인 헤더 블러 기능 추가
 $(window).scroll(function () {
-    if ($(this).scrollTop() > 1){
-        $('#main-nav').addClass('sticky')
-    }
-    else {
-        $('#main-nav').removeClass('sticky')
-    }
-})
+  $( "#main-nav" ).addClass( "blurredclass" );
+});
 
 
+//모바일 적용 시 햄버거 메뉴 클릭 버튼 활성화
 $(document).ready(function () {
     $('.burger').click(function () {
         $('.navbar-menu').toggleClass('active')
