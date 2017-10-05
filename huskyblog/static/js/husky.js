f6772b9d21d6161131b79ff9f97029480aafd1ba
huskyhoochu <dfg1499@gmail.com>
2017-10-05 16:20:39 +0900

    타이포그래피

--- huskyblog/static/js/husky.js
+++ huskyblog/static/js/husky.js
@@ -1,9 +1,3 @@
-//메인 헤더 블러 기능 추가
-$(window).scroll(function () {
-  $( "#main-nav" ).addClass( "blurredclass" );
-});
-
-
 //모바일 적용 시 햄버거 메뉴 클릭 버튼 활성화
 $(document).ready(function () {
     $('.burger').click(function () {
