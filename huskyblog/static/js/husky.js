1d24709f7d8aba03c550d5817699bfd731656ee6
huskyhoochu <dfg1499@gmail.com>
2017-10-05 14:50:32 +0900

    메인화면 컨텐츠 슬라이스 적용 및 css 수정

--- huskyblog/static/js/husky.js
+++ huskyblog/static/js/husky.js
@@ -1,3 +1,13 @@
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 1){
+        $('#main-nav').addClass('sticky')
+    }
+    else {
+        $('#main-nav').removeClass('sticky')
+    }
+})
+
+
 $(document).ready(function () {
     $('.burger').click(function () {
         $('.navbar-menu').toggleClass('active')
