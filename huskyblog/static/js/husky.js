3ecde8c8bdd6865bab96d716a09dbba205276db3
huskyhoochu <dfg1499@gmail.com>
2017-10-05 18:35:55 +0900

    세부 변경

--- huskyblog/static/js/husky.js
+++ huskyblog/static/js/husky.js
@@ -1,3 +1,9 @@
+//메인 헤더 쉐도우 기능 추가
+$(window).scroll(function () {
+  $( "#main-nav" ).css('box-shadow', '0 3px 13px 0 rgba(2, 3, 3, 0.16)');
+});
+
+
 //모바일 적용 시 햄버거 메뉴 클릭 버튼 활성화
 $(document).ready(function () {
     $('.burger').click(function () {
